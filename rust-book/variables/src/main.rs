8b8eefc57837075c5469e99632a6e8c80b49a120
Dayne <dayne@code.org>
2026-02-26 23:43:48 -0800

    3.2 Data Types

---
+++ rust-book/variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    let tup = (500, 6.4, 1);
+    println!("The value of tup is: {:?}", tup);
+}
