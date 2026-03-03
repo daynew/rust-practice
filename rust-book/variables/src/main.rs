e4f2b41804f89c25f181bcd904ba30efc89682ac
Dayne <dayne@code.org>
2026-03-02 23:00:26 -0800

    Finished 3.2 Data Types

--- rust-book/variables/src/main.rs
+++ rust-book/variables/src/main.rs
@@ -3,6 +3,16 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+    // Tuple
     let tup = (500, 6.4, 1);
     println!("The value of tup is: {:?}", tup);
+
+    // Array types
+
+    // Array of zeros
+    let zeros = [0; 10];
+    println!("Array of zeroes {:?}", zeros);
+
+    let inc = [1, 2, 3];
+    println!("inc={:?} inc[0]={1}", inc, inc[0]);
 }
