ca52260ee3c68f82b5cfd0299bca954588b300c4
Dayne <dayne@code.org>
2026-03-06 18:26:27 -0800

    Removing a little rust builder thing I wasn't using

--- new_project_setup/.nvim.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = "*.rs",
-    callback = function()
-        -- Runs 'make' silently and populates the quickfix list
-        vim.cmd("silent make!")
-    end,
-})
