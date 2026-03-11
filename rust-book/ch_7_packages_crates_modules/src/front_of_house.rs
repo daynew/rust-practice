5acf60149f446f52e54900216fb6fe0a0a130469
Dayne <dayne@code.org>
2026-03-11 11:51:04 -0700

    Finished 7. Packages, Crates, and Modules

---
+++ rust-book/ch_7_packages_crates_modules/src/front_of_house.rs
@@ -0,0 +1,9 @@
+pub mod hosting;
+
+mod serving {
+    fn take_over() {}
+
+    fn serve_order() {}
+
+    fn take_payment() {}
+}
---
+++ rust-book/ch_7_packages_crates_modules/src/lib.rs
@@ -0,0 +1,36 @@
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    let mut meal = crate::back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
