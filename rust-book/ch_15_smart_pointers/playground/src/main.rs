132596fb69b3ee8055b767f27550777f3b650d74
Dayne <dayne@code.org>
2026-03-18 17:41:18 -0700

    Learned about deref() and drop()

--- rust-book/ch_15_smart_pointers/playground/src/main.rs
+++ rust-book/ch_15_smart_pointers/playground/src/main.rs
@@ -3,6 +3,11 @@ fn main() {
     println!("b = {b}");
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    use_box_like_a_ref();
+
+    let m = MyBox::new(String::from("Dayne"));
+    hello(&m);
+    drop_test();
 }
 
 use crate::List::{Cons, Nil};
@@ -10,4 +15,53 @@ use crate::List::{Cons, Nil};
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+fn use_box_like_a_ref() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}");
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn drop_test() {
+    let c = CustomSmartPointer {
+        data: String::from("my c stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other d stuff"),
+    };
+    println!("CustomSmartPointers created");
 }
